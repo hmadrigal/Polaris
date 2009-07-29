1872d2479ae4f1936977f65610860e680b459d17
abakerp_cp <4a570eb1efc1ae6599a962042706b207c767074bHUtyGRvF>
2009-07-29 22:14:05 +0000

    Adding Basic Polaris Project Structure and associated projects
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C36583

---
+++ Polaris.Portal/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Polaris.Pal.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        #region Fields
+        #endregion
+
+        #region Actions
+
+        // GET: /
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to Polaris Gamming Experiment!";
+
+            return View();
+        }
+
+        // GET: /About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
