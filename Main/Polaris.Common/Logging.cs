960ce1c5802cd06b8287764605ee4d9018c71746
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2009-07-30 22:40:38 +0000

    Added Logging class. This class has helper methods for writing log entries.
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C36832

--- Main/Polaris.Common/Logging.cs
+++ Main/Polaris.Common/Logging.cs
@@ -8,45 +8,97 @@ using System.Diagnostics;
 
 namespace Polaris.Common
 {
+    /// <summary>
+    /// This class helps to write log messages
+    /// </summary>
     public static class Logging
     {
-
+        /// <summary>
+        /// Event fired when there was an error when trying to write an entry in the log file
+        /// </summary>
         public static event EventHandler<EventArgs> LoggingFailed;
 
+        /// <summary>
+        /// Default EventId when writing entries into the log
+        /// </summary>
         private static readonly int defaultEventId;
+        /// <summary>
+        /// Default Priority when writing entries into the log
+        /// </summary>
         private static readonly int defaultPriority;
+        /// <summary>
+        /// Default Categories when writing entries into the log
+        /// </summary>
         private static readonly string[] defaultCategories;
 
+        /// <summary>
+        /// Initializes the default values 
+        /// </summary>
         static Logging()
         {
             Int32.TryParse(ConfigurationManager.AppSettings["Polaris.Common.Logging.Default.EventId"], out defaultEventId);
             Int32.TryParse(ConfigurationManager.AppSettings["Polaris.Common.Logging.Default.Priority"], out defaultPriority);
-            defaultCategories = (ConfigurationManager.AppSettings["Polaris.Common.Logging.Default.Priority"] as String).Split(';');
+            defaultCategories = (ConfigurationManager.AppSettings["Polaris.Common.Logging.Default.Categories"] as string).Split(';');
+            //defaultCategories = ((String)ConfigurationManager.AppSettings["Polaris.Common.Logging.Default.Categories"] ?? String.Empty).Split(';');
         }
 
-        public static void LogError(String message)
+        /// <summary>
+        /// Logs a error messages 
+        /// </summary>
+        /// <param name="message"></param>
+        public static void LogError(String formatMessage, params object[] args)
         {
-            LogMessage(message, TraceEventType.Error);
+            LogMessage(TraceEventType.Error, formatMessage, args);
         }
 
-        public static void LogInformation(String message)
+        /// <summary>
+        /// Logs a information message
+        /// </summary>
+        /// <param name="message"></param>
+        public static void LogInformation(String formatMessage, params object[] args)
         {
-            LogMessage(message, TraceEventType.Information);
+            LogMessage(TraceEventType.Information, formatMessage, args);
         }
 
-        public static void LogWarning(String message)
+        /// <summary>
+        /// Logs a warninig message
+        /// </summary>
+        /// <param name="message"></param>
+        public static void LogWarning(String formatMessage, params object[] args)
         {
-            LogMessage(message, TraceEventType.Error);
+            LogMessage(TraceEventType.Error, formatMessage, args);
         }
 
-        public static void LogMessage(String message, TraceEventType severity)
+        /// <summary>
+        /// Allows to log a message and specify the severity of the message
+        /// </summary>
+        /// <param name="message">message</param>
+        /// <param name="severity">severity</param>
+        public static void LogMessage(TraceEventType severity, String formatMessage, params object[] args)
         {
-            LogMessage(message, defaultEventId, defaultPriority, severity, defaultCategories);
+            LogMessage(defaultEventId, defaultPriority, severity, formatMessage, args);
         }
-        public static void LogMessage(String message, int eventId, int priority, TraceEventType severity)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="eventId"></param>
+        /// <param name="priority"></param>
+        /// <param name="severity"></param>
+        public static void LogMessage(int eventId, int priority, TraceEventType severity, String formatMessage, params object[] args)
         {
-            LogMessage(message, eventId, priority, severity, defaultCategories);
+            LogMessage(String.Format(formatMessage, args), eventId, priority, severity, defaultCategories);
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="eventId"></param>
+        /// <param name="priority"></param>
+        /// <param name="severity"></param>
+        /// <param name="categories"></param>
         public static void LogMessage(String message, int eventId, int priority, TraceEventType severity, params String[] categories)
         {
             try
@@ -74,6 +126,10 @@ namespace Polaris.Common
             }
         }
 
+        /// <summary>
+        /// Fires the 'LoggingFailed' event. 
+        /// </summary>
+        /// <param name="ex"></param>
         private static void OnLoggingFailed(Exception ex)
         {
             if (LoggingFailed != null)
