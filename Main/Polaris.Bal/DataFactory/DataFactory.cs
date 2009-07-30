39c6b3fb3fcaca125742120d9f282525b0fde78f
abakerp_cp <4a570eb1efc1ae6599a962042706b207c767074bHUtyGRvF>
2009-07-30 21:24:34 +0000

    Checking in base DataFactory guide class and creating basic SiteRepository test class
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C36822

---
+++ Main/Polaris.Bal/DataFactory/DataFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Polaris.Bal.Helpers.Settings;
+using System.Reflection;
+
+namespace Polaris.Bal
+{
+    /// <summary>
+    /// This class serves to create instances of the Data Abstraction Layer data provider classes
+    /// </summary>
+    public class DataFactory
+    {
+        #region Fields
+
+        // Look up the DAL implementation we should be using
+        private static readonly string path = AppSettings.DalAssembly;
+
+        #endregion
+
+        #region Constructor
+
+        // Default constructor
+        private DataFactory() { }
+
+        #endregion
+
+        #region Provider Creator Methods
+
+        /// <summary>
+        /// Creates an instance of the Data Abstraction Layer class
+        /// </summary>
+        /// <param name="dalName">the name of the dal class</param>
+        /// <returns>an instance of the dal class</returns>
+        public static IRepository CreateDAL(string dalName)
+        {
+            string className = path + "." + dalName;
+            return (IRepository)Assembly.Load(path).CreateInstance(className);
+        }
+
+        /// <summary>
+        /// Creates an instance of the Data Abstraction Layer Sponsor class
+        /// </summary>
+        /// <returns>an instance of the dal Sponsor class</returns>
+        public static ISiteRepository CreateSiteRepository()
+        {
+            string className = path + ".SiteRepository";
+            return (ISiteRepository)Assembly.Load(path).CreateInstance(className);
+        }
+
+
+        #endregion
+    }
+}
---
+++ Tests/Polaris.Tal/ISiteRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using Polaris.Bal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Polaris.Portal.Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ISiteRepositoryTest and is intended
+    ///to contain all ISiteRepositoryTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ISiteRepositoryTest
+    {
+        internal virtual ISiteRepository CreateISiteRepository()
+        {
+            // TODO: Instantiate an appropriate concrete class.
+            ISiteRepository target = null;
+            return target;
+        }
+
+        /// <summary>
+        ///A test for GetUsers
+        ///</summary>
+        [TestMethod()]
+        public void GetUsersTest()
+        {
+            ISiteRepository target = CreateISiteRepository(); 
+            IEnumerable<IUser> actual;
+            actual = target.GetUsers();
+            Assert.AreNotEqual(null, actual);
+        }
+    }
+}
