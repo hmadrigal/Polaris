1ca89e1a838518028dd6703a6f3ebd01a0a81b6a
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2009-08-03 20:17:55 +0000

    This change was pending in my previous check in. It's the new password column into the User table
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C37960

--- Main/Polaris.Dbl/Schema Objects/Tables/Polaris.User.table.sql
+++ Main/Polaris.Dbl/Schema Objects/Tables/Polaris.User.table.sql
@@ -2,6 +2,7 @@
 (
 [UserId] [bigint] NOT NULL IDENTITY(1, 1),
 [Username] [varchar] (20) NOT NULL,
+[Password] [varchar] (50) NOT NULL,
 [FirstName] [varchar] (30) NOT NULL,
 [LastName] [varchar] (30) NOT NULL,
 [Email] [varchar] (100) NOT NULL,
