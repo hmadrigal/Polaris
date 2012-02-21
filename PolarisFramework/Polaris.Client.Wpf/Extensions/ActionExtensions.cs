2204720e9c861ecc6b9a10120ece2e54dc633209
murven_cp <691d12b833780a75966e9a914607b54f61e8bfbdQ9gqpGfQ>
2012-02-21 15:39:09 +0000

    * Adding the Action.InvokeWithDelay extension method.
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C88565

--- PolarisFramework/Polaris.Client.Wpf/Extensions/ActionExtensions.cs
+++ PolarisFramework/Polaris.Client.Wpf/Extensions/ActionExtensions.cs
@@ -1,16 +1,38 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="ActionExtensions.cs" company="">
+// <copyright file="ActionExtensions.cs" company="Polaris Community">
 //     This code is distributed under the Microsoft Public License (MS-PL).
 // </copyright>
 //-----------------------------------------------------------------------
 namespace Polaris.Client.Wpf.Extensions
 {
     using System;
+    using System.Threading;
     using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
     using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+    using System.Windows.Threading;
 
     public static class ActionExtensions
     {
+        public static void InvokeWithDelay(this Action delayedAction, TimeSpan delay, Dispatcher sourceThreadDispatcher = null)
+        {
+            if (delayedAction == null) { return; }
+            ThreadPool.QueueUserWorkItem(new WaitCallback((target) =>
+            {
+                Thread.Sleep(delay);
+                if (sourceThreadDispatcher != null)
+                {
+                    sourceThreadDispatcher.BeginInvoke(new Action(() =>
+                    {
+                        delayedAction();
+                    }));
+                }
+                else
+                {
+                    delayedAction();
+                }
+            }));
+        }
+
         public static bool TryInvoke(this Action targetAction)
         {
             try
