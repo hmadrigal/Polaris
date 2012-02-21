a1f1ce4229ac22ea23269e1b94eb2c55e1408f45
murven_cp <691d12b833780a75966e9a914607b54f61e8bfbdQ9gqpGfQ>
2012-02-21 18:09:59 +0000

    * Adding BooleanToVisibilityConverter
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C88583

---
+++ PolarisFramework/Polaris.Client.Controls.Wpf/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,51 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BooleanToVisibilityConverter.cs" company="Polaris Community">
+//     This code is distributed under the Microsoft Public License (MS-PL).
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Polaris.Windows.Converters
+{
+    using System;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Ink;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+    using System.Windows.Data;
+    using System.Globalization;
+
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(Visibility))
+            {
+                var visible = System.Convert.ToBoolean(value, culture);
+                if (InvertVisibility)
+                    visible = !visible;
+                return visible ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else if (targetType == typeof(String))
+            {
+                var visible = System.Convert.ToBoolean(value, culture);
+                if (InvertVisibility)
+                    visible = !visible;
+                return visible ? Visibility.Visible.ToString() : Visibility.Collapsed.ToString();
+            }
+            throw new InvalidOperationException("Converter can only convert to value of type Visibility and String");
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("Converter cannot convert back.");
+        }
+
+        public Boolean InvertVisibility { get; set; }
+
+    }
+}
