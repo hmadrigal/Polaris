4b0b985f7937d0dd369610a5ffdeef1c94ffa1d1
wrojas_cp <1782fa0bb7df5516115e176b79c5bf615b627da1Ab3AFE7t>
2012-05-03 20:34:42 +0000

    * Adding ViewRegistryService to Polaris framework.
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C90989

---
+++ Develop/Polaris.Wpf.ViewRegistry/ViewRegistryService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Microsoft.Practices.Unity;
+
+namespace Polaris.Windows.Services
+{
+    public class ViewRegistryService : IViewRegistryService
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        public void RegisterView<T>(string name, bool isSingleton = true) where T : FrameworkElement
+        {
+            if (isSingleton)
+            {
+                Container.RegisterType(typeof(T), name, new ContainerControlledLifetimeManager());
+            }
+            else
+            {
+                Container.RegisterType(typeof(T), name);
+            }
+        }
+
+        public FrameworkElement ResolveView(string name)
+        {
+            var registration = Container.Registrations.Where(t => !String.IsNullOrEmpty(t.Name) && t.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            if (registration == null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionStrings.ViewIsNotRegistered, name));
+            }
+            var view = Container.Resolve(registration.RegisteredType) as FrameworkElement;
+            return view;
+        }
+    }
+}
