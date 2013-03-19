a711dcbf1812815e2cf67af19299c49c4c0babbf
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-03-19 01:54:45 +0000

    Using Displayname as DP instead of CLR property
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C100899

--- Develop/Source/Wpf/Polaris.Wpf.Ui/LogicalKeys/ILogicalKey.cs
+++ Develop/Source/Wpf/Polaris.Wpf.Ui/LogicalKeys/ILogicalKey.cs
@@ -17,7 +17,29 @@
         public event LogicalKeyPressedEventHandler LogicalKeyPressed;
 
         public IKeyboardInput KeyboardService { get; set; }
-        public object DisplayName { get; set; }
+        //public object DisplayName { get; set; }
+        #region DisplayName
+
+        /// <summary>
+        /// DisplayName Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty DisplayNameProperty =
+            DependencyProperty.Register("DisplayName", typeof(object), typeof(DependencyLogicalKey),
+                new FrameworkPropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets the DisplayName property.  This dependency property 
+        /// indicates ....
+        /// </summary>
+        public object DisplayName
+        {
+            get { return (object)GetValue(DisplayNameProperty); }
+            set { SetValue(DisplayNameProperty, value); }
+        }
+
+        #endregion
+
+        
 
         public DependencyLogicalKey()
         {
@@ -32,7 +54,8 @@
 
         protected void OnKeyPressed()
         {
-            if (LogicalKeyPressed != null) LogicalKeyPressed(this, new LogicalKeyEventArgs(this));
+            var threadSafeLogicalKeyPressed = LogicalKeyPressed;
+            if (threadSafeLogicalKeyPressed != null) threadSafeLogicalKeyPressed(this, new LogicalKeyEventArgs(this));
         }
     }
 
