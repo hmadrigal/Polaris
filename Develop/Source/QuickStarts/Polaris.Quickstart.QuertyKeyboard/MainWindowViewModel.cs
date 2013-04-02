4ca13f30fb6e99c4d4d401756be51e7f02bae189
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-04-02 22:08:48 +0000

    Sharing binding for Shift Keys
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C101247

---
+++ Develop/Source/QuickStarts/Polaris.Quickstart.QuertyKeyboard/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Polaris.Quickstart.QuertyKeyboard
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region IsShiftEnabled (INotifyPropertyChanged Property)
+        public bool IsShiftEnabled
+        {
+            get { return _isShiftEnabled; }
+            set
+            {
+                if (_isShiftEnabled != value)
+                {
+                    _isShiftEnabled = value;
+                    RaisePropertyChanged("IsShiftEnabled");
+                }
+            }
+        }
+        private bool _isShiftEnabled;
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var threadSafePropertyChanged = PropertyChanged;
+            if (threadSafePropertyChanged != null)
+            {
+                threadSafePropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
