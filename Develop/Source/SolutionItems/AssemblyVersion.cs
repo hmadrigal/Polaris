bbe08c7e66f4f72b2f0bba4dca167464389f657d
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-04-02 18:53:04 +0000

    Updating nuget files properly (the Inline task was trimming one digit of the version number)
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C101243

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.16473.48848")]
+[assembly: System.Reflection.AssemblyVersion("1.0.16480.51515")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
