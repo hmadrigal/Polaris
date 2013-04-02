560aafda342c5a22eb9124e37222a16e38832bd5
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-04-02 17:55:35 +0000

    Automating nuspec version numering based on AssemblyVersion.cs (which is also computed)
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C101242

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.16469.47641")]
+[assembly: System.Reflection.AssemblyVersion("1.0.16473.48848")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
