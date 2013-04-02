15d84ec57afa4fa2d94f964224dbc64015fd9e5e
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-04-02 17:27:58 +0000

    Added Task for automating version number of the AssemblyVersion.cs (removed kinect projects from SLN)
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C101241

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.1304.4534")]
+[assembly: System.Reflection.AssemblyVersion("1.0.16469.47641")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
