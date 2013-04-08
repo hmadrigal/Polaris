994e99459e7b9f2e48035eb3fe4bab70c27a2d5f
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-04-08 19:14:38 +0000

    Added project sample for CAL phone app.
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C101335

---
+++ Develop/Source/QuickStarts/Polaris.Quickstart.CalPhoneApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Polaris.Quickstart.CalPhoneApp.Resources;
+
+namespace Polaris.Quickstart.CalPhoneApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
