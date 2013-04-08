b80e1047486e1b14a6be23f2dae9fcf78ed4c77d
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-04-08 21:07:06 +0000

    Setting up properly the namespaces references.
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C101338

---
+++ Develop/Source/Phone/Polaris.PhoneLib.Toolkit/Messages/AppLifeCycleMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polaris.PhoneLib.Toolkit.Messages
+{
+    public class AppLifeCycleMessage
+    {
+        public LifeCycleState Status { get; private set; }
+
+        public AppLifeCycleMessage(LifeCycleState status)
+        {
+            Status = status;
+        }
+    }
+}
---
+++ Develop/Source/Phone/Polaris.PhoneLib.Toolkit/Messages/LifeCycleState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polaris.PhoneLib.Toolkit.Messages
+{
+    public enum LifeCycleState
+    {
+        ActivatingFromDormant,
+        ActivatingFromTombstone,
+        Deactivating,
+    }
+}
