2f0afc04865ce409893c1132defd375cdbfa700b
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-05-06 20:34:29 +0000

    Deploying Phone Version
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C101804

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.22594.55013")]
+[assembly: System.Reflection.AssemblyVersion("1.0.22597.56143")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
