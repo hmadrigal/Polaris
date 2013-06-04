5b8635388ff05d5940d8f44d1421ef941094c835
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-06-04 04:45:27 +0000

    Updating common package
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C102244

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.27676.9283")]
+[assembly: System.Reflection.AssemblyVersion("1.0.27686.12943")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
