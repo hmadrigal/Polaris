9a6262cee3dbf772b7ab204cb8b6f0dbc2018957
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-07-03 02:33:41 +0000

    Release 1.0.32872.5337
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C102788

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.32872.5230")]
+[assembly: System.Reflection.AssemblyVersion("1.0.32872.5337")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
