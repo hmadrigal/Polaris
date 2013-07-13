0856682ad534e510fc06cd5ad935b8a53179356f
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-07-13 02:39:02 +0000

    Changing automatically version of Polaris.build
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C102914

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.34670.6254")]
+[assembly: System.Reflection.AssemblyVersion("1.0.34672.7202")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
