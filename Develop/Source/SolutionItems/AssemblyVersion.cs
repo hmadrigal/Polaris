83780fc76c119b72f5c75806abb3823d176fc044
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-07-13 02:00:25 +0000

    Removing solution path reference, to use local path
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C102910

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.32872.5337")]
+[assembly: System.Reflection.AssemblyVersion("1.0.34667.5381")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
