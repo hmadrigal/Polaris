d9488e341ddddc9c1dce232270b59b16320aff16
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-07-13 02:28:49 +0000

    Creating Polaris.Build package
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C102911

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.34667.5381")]
+[assembly: System.Reflection.AssemblyVersion("1.0.34670.6254")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
