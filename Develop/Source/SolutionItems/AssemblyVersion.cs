6d42c6c5d4a958dbd855f34906196cc0e8c65b58
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-07-25 18:31:45 +0000

    Preparing for deployment 1.0.36946.50557
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C103046

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.36944.49767")]
+[assembly: System.Reflection.AssemblyVersion("1.0.36946.50557")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
