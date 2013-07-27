e5f31fe88008f3c033f2e7823f8c382e3edf5e9a
hmadrigal_cp <3e4fdc78e7f6b41b064b270d5d2035bc7f8a92886y2SPJQA>
2013-07-27 16:36:24 +0000

    Deploying new version. Updating Mvvm depedency
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs04]$/polaris;C103091

--- Develop/Source/SolutionItems/AssemblyVersion.cs
+++ Develop/Source/SolutionItems/AssemblyVersion.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.37289.44585")]
+[assembly: System.Reflection.AssemblyVersion("1.0.37291.45298")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright Polaris 2013")]
